d50b18280e72f34b1a5323b076724d383460f244
Yuri <yurharutyunyan0@gmail.com>
2023-08-09 12:16:37 +0400

    first commit of second homework

---
+++ JsPracticalHomework1/homeworkNo2/app.js
@@ -0,0 +1,15 @@
+Array.prototype.unshiftAnArray = function unshiftAnArray(arr) {
+  let resultingArray = [];
+  if (Array.isArray(arr)) {
+    resultingArray = arr;
+  } else {
+    resultingArray = [arr];
+  }
+  for (let i = resultingArray.length - 1; i >= 0; i--) {
+    this.splice(0, 0, resultingArray[i]);
+  }
+  console.log(resultingArray);
+};
+let array = [1, 2, 3];
+array.unshiftAnArray([4, 5, 6, 7]);
+console.log(array);
